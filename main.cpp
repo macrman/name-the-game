5dcb150417d7a66d01eb08a00b33f1f78086194e
Michael Ackerman <binarymac@gmail.com>
2014-10-01 18:38:43 -0700

    box now moves up/down/left/right

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@ int main()
     // create a window
     sf::RenderWindow window(sf::VideoMode(1024, 768), "My Game Window Title");
 
+    // initiate a rectangle
+    sf::RectangleShape myrectangle(sf::Vector2f(64, 96));
+
+    // fill the rectangle with yellow
+    myrectangle.setFillColor(sf::Color(255,255,0));
+
+    // set position
+    int x = 50;
+    int y = 100;
+    myrectangle.setPosition(x, y);
+
     // main game loop
     while (window.isOpen())
     {
@@ -20,10 +31,33 @@ int main()
             }
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            x -= 1;
+            myrectangle.setPosition(x, y);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            y -= 1;
+            myrectangle.setPosition(x, y);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            x += 1;
+            myrectangle.setPosition(x, y);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            y+= 1;
+            myrectangle.setPosition(x, y);
+        }
+
         // clears the window.. MANDATORY!
         window.clear(sf::Color::Black);
 
         // draw stuff;
+        // draw the rectangle
+        window.draw(myrectangle);
 
         // ends the frame, and draws everything. MANDATORY!
         window.display();
